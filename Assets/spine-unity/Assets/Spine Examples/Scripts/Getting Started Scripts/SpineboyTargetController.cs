54e1260c4aff19d7d7aad6c6b46cdcc4771c3845
kira0102 <93621737@qq.com>
2018-08-11 14:47:44 +0800

    add Spine

---
+++ Assets/spine-unity/Assets/Spine Examples/Scripts/Getting Started Scripts/SpineboyTargetController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Spine.Unity.Examples {
+	public class SpineboyTargetController : MonoBehaviour {
+
+		public SkeletonAnimation skeletonAnimation;
+
+		[SpineBone(dataField:"skeletonAnimation")]
+		public string boneName;
+		public new Camera camera;
+
+		Bone bone;
+
+		void OnValidate () {
+			if (skeletonAnimation == null) skeletonAnimation = GetComponent<SkeletonAnimation>();
+		}
+
+		void Start () {
+			bone = skeletonAnimation.Skeleton.FindBone(boneName);
+//			skeletonAnimation.skeleton.FindIkConstraint ("aiming gun constraint").Mix = 1;
+		}
+
+		void Update () {
+			var mousePosition = Input.mousePosition;
+			var worldMousePosition = camera.ScreenToWorldPoint(mousePosition);
+			var skeletonSpacePoint = skeletonAnimation.transform.InverseTransformPoint(worldMousePosition);
+			if (skeletonAnimation.Skeleton.FlipX) skeletonSpacePoint.x *= -1;
+			bone.SetPosition(skeletonSpacePoint);
+		}
+	}
+
+}
