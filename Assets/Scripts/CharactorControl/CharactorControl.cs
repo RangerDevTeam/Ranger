4350263bfc77b3d7186f0f7790c7e6d8478f35b2
tcrzxp <tcrzxp@163.com>
2018-08-11 14:15:25 +0800

    提交按键管理

---
+++ Assets/Scripts/CharactorControl/CharactorControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(Keyboard.moveUp))
+        {
+            transform.Translate(Vector3.up * 2f * Time.deltaTime);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/CharactorControl/Keyboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keyboard{
+
+    public static KeyCode moveUp = KeyCode.W;
+    public static KeyCode moveLeft = KeyCode.A;
+    public static KeyCode moveRight = KeyCode.D;
+    public static KeyCode moveDown = KeyCode.S;
+    public static List<KeyCode> skillKeyCode = new List<KeyCode>();
+
+
+}
