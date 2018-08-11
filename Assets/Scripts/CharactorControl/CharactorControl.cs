07a2bf5a656d99a18f2fe00402e88f7acf2b2aeb
tcrzxp <tcrzxp@163.com>
2018-08-11 14:21:45 +0800

    添加角色的移动效果“
    ddd
    
    qw
    :qw
    添加角色的移动效果“

--- Assets/Scripts/CharactorControl/CharactorControl.cs
+++ Assets/Scripts/CharactorControl/CharactorControl.cs
@@ -12,11 +12,36 @@ public class CharactorControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        ControlPlayer();
 
+	}
+
+
+    //要考虑下是否有更好的写法
+    //通过输入的按键控制玩家
+    void ControlPlayer()
+    {
+        //向上走
         if (Input.GetKey(Keyboard.moveUp))
         {
             transform.Translate(Vector3.up * 2f * Time.deltaTime);
         }
+        //向下走
+        if (Input.GetKey(Keyboard.moveDown))
+        {
+            transform.Translate(Vector3.down * 2f * Time.deltaTime);
+        }
+        //向左走
+        if (Input.GetKey(Keyboard.moveLeft))
+        {
+            transform.Translate(Vector3.left * 2f * Time.deltaTime);
+        }
+        //向右走
+        if (Input.GetKey(Keyboard.moveRight))
+        {
+            transform.Translate(Vector3.right * 2f * Time.deltaTime);
+        }
 		
-	}
+    }
+
 }
